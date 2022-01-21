5f515f410108f4b18b46b53c0f773e2ef3344680
Pippijn van Steenhoven <root@xinutec.org>
2022-01-21 12:17:25 +0000

    Initial commit.

---
+++ base-configuration.nix
@@ -0,0 +1,21 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "/dev/sda";
+
+  boot.cleanTmpDir = true;
+  zramSwap.enable = true;
+
+  virtualisation.docker.enable = true;
+
+  programs.neovim.enable = true;
+  programs.neovim.viAlias = true;
+}
