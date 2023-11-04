afef319e285665afca4d60b932db579dca476726
Pippijn van Steenhoven <root@xinutec.org>
2023-11-04 15:57:33 +0000

    Simplify odin config.

--- machines/isis/configuration.nix
+++ machines/isis/configuration.nix
@@ -13,12 +13,12 @@ in {
     enable = true;
     role = "agent";
     tokenFile = "/root/node-token";
-    serverAddr = "https://${net.nodes.amun.vpn}:${toString net.k8sApiPort}";
+    serverAddr = "https://${net.nodes.master.vpn}:${toString net.k8sApiPort}";
     extraFlags = "--node-ip ${config.node.vpn} --flannel-iface=wg0";
   };
 
   fileSystems."/export" = {
-    device = "${net.nodes.amun.vpn}:/export";
+    device = "${net.nodes.master.vpn}:/export";
     fsType = "nfs4";
   };
 }
--- machines/odin/configuration.nix
+++ machines/odin/configuration.nix
@@ -8,34 +8,8 @@ let net = import ../../network.nix;
 in {
   imports = [ ../../base-configuration.nix ];
 
-  # Enable WireGuard
-  networking.wireguard.interfaces = {
-    # "wg0" is the network interface name. You can name the interface arbitrarily.
-    wg0 = {
-      peers = [
-        # For a client configuration, one peer entry for the server will suffice.
-        {
-          # Public key of the server (not a file path).
-          publicKey = net.nodes.amun.publicKey;
-
-          # Forward all the traffic via VPN.
-          #allowedIPs = [ "0.0.0.0/0" ];
-          # Or forward only particular subnets
-          allowedIPs = [ net.vpn ];
-
-          # Set this to the server IP and port.
-          # TODO: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
-          endpoint = "${net.nodes.amun.ipv4}:${toString net.vpnPort}";
-
-          # Send keepalives every 25 seconds. Important to keep NAT tables alive.
-          persistentKeepalive = 25;
-        }
-      ];
-    };
-  };
-
   fileSystems."/export" = {
-    device = "${net.nodes.amun.vpn}:/export";
+    device = "${net.nodes.master.vpn}:/export";
     fsType = "nfs4";
   };
 }
