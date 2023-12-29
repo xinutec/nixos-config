0c9c22e4030a56cd9664f3caf0d45fafbb6e6495
Pippijn van Steenhoven <root@xinutec.org>
2023-12-29 11:49:48 +0000

    Move dev container to isis.

--- machines/amun/configuration.nix
+++ machines/amun/configuration.nix
@@ -39,24 +39,6 @@ in {
   };
 
   virtualisation.oci-containers.containers = {
-    toktok = {
-      image = "xinutec/toktok:latest";
-      ports = [ "2223:22" ];
-      extraOptions = [
-        "--tmpfs=/run"
-        "--tmpfs=/run/wrappers:exec,suid"
-        "--tmpfs=/tmp:exec"
-      ];
-      volumes = [
-        "/sys/fs/cgroup:/sys/fs/cgroup"
-        "${config.users.users.pippijn.home}/code/kubes/vps/toktok/home/.config/tox:/home/builder/.config/tox"
-        "${config.users.users.pippijn.home}/code/kubes/vps/toktok/workspace:/src/workspace"
-        "${config.users.users.pippijn.home}/.local/share/vscode/config:/src/workspace/.vscode"
-        "${config.users.users.pippijn.home}/.local/share/vscode/server:/home/builder/.vscode-server"
-        "${config.users.users.pippijn.home}/.local/share/zsh/toktok:/home/builder/.local/share/zsh"
-      ];
-    };
-
     buildfarm-server = {
       image = "toxchat/buildfarm-server";
       dependsOn = [ "buildfarm-redis" ];
