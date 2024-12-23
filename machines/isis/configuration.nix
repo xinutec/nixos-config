baf57ad8c11663a150d5446207bf607117ce0efd
Pippijn van Steenhoven <root@xinutec.org>
2024-12-23 17:00:47 +0000

    Turn isis into k8s master.

--- machines/isis/configuration.nix
+++ machines/isis/configuration.nix
@@ -8,17 +8,34 @@ let net = import ../../network.nix;
 in {
   imports = [ ../../base-configuration.nix ];
 
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    kubectl # to manage kubernetes
+    kubernetes-helm # to install kubernetes packages (helm charts)
+  ];
+
+  networking.firewall.allowedTCPPorts = [ 2223 28192 ];
+
   # List services that you want to enable:
   services.k3s = {
     enable = true;
-    role = "agent";
-    tokenFile = "/root/node-token";
-    serverAddr = "https://${net.nodes.master.vpn}:${toString net.k8sApiPort}";
-    extraFlags = "--node-ip ${config.node.vpn} --flannel-iface=wg0";
+    role = "server";
+    extraFlags =
+      "--disable traefik --advertise-address ${config.node.vpn} --flannel-iface=wg0";
   };
 
-  fileSystems."/export/home" = {
-    device = "${net.nodes.master.vpn}:/export/home";
-    fsType = "nfs4";
-  };
+  # List services that you want to enable:
+#  services.k3s = {
+#    enable = true;
+#    role = "agent";
+#    tokenFile = "/root/node-token";
+#    serverAddr = "https://${net.nodes.master.vpn}:${toString net.k8sApiPort}";
+#    extraFlags = "--node-ip ${config.node.vpn} --flannel-iface=wg0";
+#  };
+
+# fileSystems."/export/home" = {
+#   device = "${net.nodes.master.vpn}:/export/home";
+#   fsType = "nfs4";
+# };
 }
